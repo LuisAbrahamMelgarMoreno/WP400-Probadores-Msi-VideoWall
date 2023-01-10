3abe5f73cfc348bf032c1c0bc073ef1dc0a3d183
LuisAbrahamMelgarMoreno <melgar.moreno.luis.ipn@gmail.com>
2023-01-10 09:04:40 -0600

    Subida completa del proyecto

---
+++ js/config.js
@@ -0,0 +1,8 @@
+var server7003 = '7003/DigitalSolution/web/',
+	server901 = '901/Sucursales/',
+	servidor = server901,
+	sucursal = "periferico-cuautitlan-bait",
+	area = "video-wall",
+	departamento = "ecommerce",
+	img = 'bait';
+//WP400
